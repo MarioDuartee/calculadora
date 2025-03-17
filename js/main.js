b020de21260a27a7536e3c0382745be5dca1e8ae
Mário Duarte <104138326+MarioDuartee@users.noreply.github.com>
2025-03-17 01:21:29 +0000

    att2

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 function calcular(tipo, valor) {
-    // função execultada a cada click
-    // se o click for do tipo acao..
+    // Função executada a cada clique
+    const resultado = document.querySelector('#resultado');
+
+    // Se o clique for do tipo ação...
     if (tipo === 'acao') {
-
         if (valor === 'c') {
-            document.querySelector('#resultado').value = ''
+            resultado.value = ''; // Limpar a tela
         }
 
-        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
-            document.querySelector('#resultado').value += valor
+        // Adicionar operações
+        if (['+', '-', '*', '/', '.'].includes(valor)) {
+            resultado.value += valor;
         }
 
+        // Calcular o resultado quando clicar no "="
         if (valor === '=') {
-            // se o click for o botao igual, pegue todo os valores que tem no campo resultado
-            // guarne na variavel valorCampo e faça o eval
-            // eval, é um comando nativo da linguagem js, no qual pega o a string e tranforma em uma operaçao js
-            // nesse caso transformou em operação matematica
-            var valorCampo = eval(document.querySelector('#resultado').value)
-
-            // mostra na tela o resultado da operaçção
-            document.querySelector('#resultado').value = valorCampo
-
+            try {
+                // Tentar calcular o valor e verificar se há erros
+                resultado.value = eval(resultado.value);
+            } catch (e) {
+                resultado.value = 'Erro'; // Em caso de erro
+            }
         }
-
-        // se o click nao for do tipo acao.. mas for tipo valor, adicione o valor no campo resultado
-    } else if (tipo === 'valor') {
-        document.querySelector('#resultado').value += valor
+    }
+    // Se o clique for do tipo valor (números), adicionar o valor no campo resultado
+    else if (tipo === 'valor') {
+        resultado.value += valor;
     }
 }
